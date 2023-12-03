5cd81d15d6be8db5e0750a02782ab6fe1ce265ee
Othy Tenk <othy.tenk@gmail.com>
2023-12-03 19:10:47 +0800

    feat: configures prettier

---
+++ prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  arrowParens: "always",
+  importOrder: ["^@/(.*)$", "^~/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+
+  bracketSameLine: true,
+  singleQuote: false,
+
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  semi: false,
+  printWidth: 80,
+};
