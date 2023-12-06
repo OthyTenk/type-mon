cf2d8806288fd9c964df819c9baf6706260214df
Othy Tenk <othy.tenk@gmail.com>
2023-12-06 19:54:38 +0800

    feat: hides some components when it is typing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import Options from "./components/Options"
 import Typing from "./components/Typing"
+import useIsTyping from "./hooks/useIsTyping"
+import { APP_NAME } from "./site_settings"
 
 const Page = () => {
+  const typing = useIsTyping()
   return (
     <>
+      <Options />
+
+      <h3
+        className={`text-2xl font-semibold ${
+          !typing.isTyping ? "text-neutral-400" : "text-transparent"
+        } w-full text-center mb-9`}>
+        {APP_NAME}
+      </h3>
       <Typing />
     </>
   )
