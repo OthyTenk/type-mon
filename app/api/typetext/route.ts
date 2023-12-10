309dfe9fb9c118a0a1e26112ee56b0c11bb8fae9
Othy Tenk <othy.tenk@gmail.com>
2023-12-10 21:06:32 +0800

    fix: removes the force-static and adds pnpxprisma generate for build command

--- app/api/typetext/route.ts
+++ app/api/typetext/route.ts
@@ -3,7 +3,7 @@ import prisma from "@/libs/prismadb"
 
 import { NextResponse } from "next/server"
 
-export const dynamic = "force-static"
+// export const dynamic = "force-static"
 
 export const POST = async (request: Request) => {
   const body = await request.json()
